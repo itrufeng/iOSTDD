ff0674fa0a04e56158d86cf92661a0898a11bbee
Jian Zhang <itrufeng@gmail.com>
2018-03-07 00:00:28 +0800

    write something description for shop cell configuration

---
+++ iOSTDDTests/ShopTableViewCellSpec.swift
@@ -0,0 +1,35 @@
+//
+//  ShopTableViewCellSpec.swift
+//  iOSTDD
+//
+//  Created by Jian Zhang on 3/6/18.
+//  Copyright © 2018 itrufeng. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ShopTableViewCellSpec: QuickSpec {
+  override func spec() {
+    describe("configure") {
+      it("shows avatar") {
+      }
+
+      it("shows name") {
+      }
+
+      it("shows rating") {
+      }
+
+      context("when there is discount") {
+        it("shows discount") {
+        }
+      }
+
+      context("when there is no discount") {
+        it("hides discount") {
+        }
+      }
+    }
+  }
+}
