6b04bb74b117a19fdd31657ea8b2fdd544368b1c
Jian Zhang <itrufeng@gmail.com>
2018-03-07 00:20:26 +0800

    finish show discount

--- iOSTDD/ShopTableViewCell.swift
+++ iOSTDD/ShopTableViewCell.swift
@@ -8,8 +8,14 @@ import UIKit
 
 class ShopTableViewCell: UITableViewCell {
   let nameLabel = UILabel(frame: .zero)
+  let discountLabel = UILabel(frame: .zero)
 
   func configure(shop: Shop) {
     nameLabel.text = shop.name
+    if let discount = shop.discount, discount.count > 0 {
+      discountLabel.isHidden = false
+    } else {
+      discountLabel.isHidden = true
+    }
   }
 }
--- iOSTDDTests/ShopTableViewCellSpec.swift
+++ iOSTDDTests/ShopTableViewCellSpec.swift
@@ -36,12 +36,39 @@ class ShopTableViewCellSpec: QuickSpec {
       }
 
       context("when there is discount") {
+        beforeEach {
+          shopCell = ShopTableViewCell(style: .default, reuseIdentifier: nil)
+          let discounts = [Discount(type: "首", message: "新用户下单立减15元")]
+          let shop = Shop(name: "黄焖鸡米饭", avatarName: "avatarIconName", rating: 4.9, discount: discounts, isExpress: false, timeReliability: false)
+          shopCell?.configure(shop: shop)
+        }
+
         it("shows discount") {
+          expect(shopCell?.discountLabel.isHidden).to(beFalse())
         }
       }
 
       context("when there is no discount") {
+        beforeEach {
+          shopCell = ShopTableViewCell(style: .default, reuseIdentifier: nil)
+          let shop = Shop(name: "黄焖鸡米饭", avatarName: "avatarIconName", rating: 4.9, discount: nil, isExpress: false, timeReliability: false)
+          shopCell?.configure(shop: shop)
+        }
+
         it("hides discount") {
+          expect(shopCell?.discountLabel.isHidden).to(beTrue())
+        }
+      }
+
+      context("when there is discount but empty") {
+        beforeEach {
+          shopCell = ShopTableViewCell(style: .default, reuseIdentifier: nil)
+          let shop = Shop(name: "黄焖鸡米饭", avatarName: "avatarIconName", rating: 4.9, discount: [], isExpress: false, timeReliability: false)
+          shopCell?.configure(shop: shop)
+        }
+
+        it("hides discount") {
+          expect(shopCell?.discountLabel.isHidden).to(beTrue())
         }
       }
     }
